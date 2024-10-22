47f56ca8a1a460f38c4f06e78fbda0f2c205a3e6
최현정 <hjchoi.dev@gmail.com>
2024-10-23 00:11:20 +0900

    feat: Add ActionBar

--- app/src/main/java/com/hyunjung/todoapp/MainActivity.kt
+++ app/src/main/java/com/hyunjung/todoapp/MainActivity.kt
@@ -1,14 +1,27 @@
 package com.hyunjung.todoapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+        navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
