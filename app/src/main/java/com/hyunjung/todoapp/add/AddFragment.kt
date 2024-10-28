f4d63d87682a6c55aff733249d83294aee8f527d
최현정 <hjchoi.dev@gmail.com>
2024-10-29 00:25:14 +0900

    feat: Create Menu for AddFragment

--- app/src/main/java/com/hyunjung/todoapp/add/AddFragment.kt
+++ app/src/main/java/com/hyunjung/todoapp/add/AddFragment.kt
@@ -1,7 +1,31 @@
 package com.hyunjung.todoapp.add
 
-import androidx.fragment.app.Fragment
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
 import com.hyunjung.todoapp.R
+import com.hyunjung.todoapp.core.BaseFragment
+import com.hyunjung.todoapp.databinding.FragmentAddBinding
 
-class AddFragment : Fragment(R.layout.fragment_add) {
+class AddFragment : BaseFragment<FragmentAddBinding>(FragmentAddBinding::inflate) {
+
+    override fun initView() {
+        setMenu()
+    }
+
+    private fun setMenu() {
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.add_fragment_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return true
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+    }
 }
