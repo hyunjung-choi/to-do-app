d235891858b2168bcfbcc387b23c690d4e0694fd
최현정 <hjchoi.dev@gmail.com>
2024-10-28 14:36:14 +0900

    feat: Create Menu for ListFragment

--- app/src/main/java/com/hyunjung/todoapp/list/ListFragment.kt
+++ app/src/main/java/com/hyunjung/todoapp/list/ListFragment.kt
@@ -1,5 +1,11 @@
 package com.hyunjung.todoapp.list
 
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.appcompat.widget.SearchView
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.navigation.fragment.findNavController
 import com.hyunjung.todoapp.R
 import com.hyunjung.todoapp.core.BaseFragment
@@ -16,5 +22,23 @@ class ListFragment : BaseFragment<FragmentListBinding>(FragmentListBinding::infl
                 findNavController().navigate(R.id.action_list_to_update)
             }
         }
+        setMenu()
+    }
+
+    private fun setMenu() {
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.list_fragment_menu, menu)
+
+                val search = menu.findItem(R.id.menu_search)
+                val searchView = search.actionView as SearchView
+                searchView.isSubmitButtonEnabled = true
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                TODO("Not yet implemented")
+            }
+        })
     }
 }
