5fa297b7a6d40bcc0c0c5c977fc1d99e42d1713e
최현정 <hjchoi.dev@gmail.com>
2024-12-16 01:52:15 +0900

    feat: Create ToDoRepository

---
+++ app/src/main/java/com/hyunjung/todoapp/data/ToDoRepository.kt
@@ -0,0 +1,12 @@
+package com.hyunjung.todoapp.data
+
+import androidx.lifecycle.LiveData
+
+class ToDoRepository(private val toDoDao: ToDoDao) {
+
+    val getAllData: LiveData<List<ToDoData>> = toDoDao.getAllData()
+
+    suspend fun insertData(toDoData: ToDoData) {
+        toDoDao.insertData(toDoData)
+    }
+}
